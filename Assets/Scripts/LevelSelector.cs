97d53f1606ffd58c627f3d08cfc2a48103c1ce21
Konrad Rozanski <41211324+KonRoz@users.noreply.github.com>
2018-12-04 22:15:11 -0600

    Level Selection Menu Linked To Other Scenes & Scroll Implemented
    I worked on the level selection menu today and was able to successfully implement a scroll bar. Scripting-wise, I was able to link each of the 5 current levels to a button on the level selection page.

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour {
+
+    //level name is inputed manually winthin the editor -- i.e. On Click Attribute for Button
+
+	public void Select(string levelName)
+    {
+        //Literal Scene name -- does not have to be the build index :)
+        SceneManager.LoadScene(levelName);
+    }
+}
